4cb0372e72ff3a8ac80564a391a4f2c8b799c3e1
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-05 22:25:25 -0300

    Show data from api

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,39 +1,73 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Form } from '@unform/web';
+import { toast } from 'react-toastify';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import api from '../../services/api';
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
 
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState<Teacher[]>([]);
+  const handleSubmit = useCallback(data => {
+    (async () => {
+      try {
+        const response = await api.get<Teacher[]>('classes', {
+          params: data,
+        });
+        setTeachers(response.data);
+      } catch (err) {
+        toast.error('Ops! Alguma coisa deu errado, tente mais tarde!');
+      }
+    })();
+  }, []);
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">
-              Matéria
-              <input id="subject" name="subject" type="text" />
-            </label>
-          </div>
+        <Form id="search-teachers" onSubmit={handleSubmit}>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
 
-          <div className="input-block">
-            <label htmlFor="week_day">
-              Dia da semana
-              <input type="text" id="week_day" />
-            </label>
-          </div>
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sabado' },
+            ]}
+          />
 
-          <div className="input-block">
-            <label htmlFor="time">
-              Hora
-              <input type="text" id="time" />
-            </label>
-          </div>
-        </form>
+          <Input type="time" name="time" label="Hora" />
+
+          <button type="submit">Buscar</button>
+        </Form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        ))}
       </main>
     </div>
   );
