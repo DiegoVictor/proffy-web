00c202ad347397d7bae44d5183328d3228feeff8
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-05 22:22:48 -0300

    Configure toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Routes from './router';
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <>
+      <ToastContainer />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
