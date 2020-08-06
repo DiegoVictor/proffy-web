19d1c3e7dde620eba277d06640b8ae99544650bd
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-05 22:25:05 -0300

    Load connections number

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import LogoImg from '../../assets/images/logo.svg';
 import LandingImg from '../../assets/images/landing.svg';
-
 import StudyIcon from '../../assets/images/icons/study.svg';
 import GiveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import PurpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
-
+import api from '../../services/api';
 import './styles.css';
 
 const Landing: React.FC = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get('connections');
+      setTotalConnections(data.total);
+    })();
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -38,7 +46,7 @@ const Landing: React.FC = () => {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={PurpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
