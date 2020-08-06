15271e3bf1d403748fd2710f960c9c01512a2bbd
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-05 22:24:14 -0300

    Create select

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,52 @@
+import React, { SelectHTMLAttributes, useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+
+import './styles.css';
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const Select: React.FC<SelectProps> = ({ name, label, options, ...props }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue = '', registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>
+        {label}
+        <select
+          id={name}
+          name={name}
+          defaultValue={defaultValue}
+          ref={inputRef}
+          {...props}
+        >
+          <option value="" disabled hidden>
+            Selecione uma opção
+          </option>
+          {options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+};
+
+export default Select;
