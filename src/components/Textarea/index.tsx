14563e39764b797fad24bd18c62ec29a10ce7b44
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-05 22:24:04 -0300

    Create textarea

---
+++ src/components/Textarea/index.tsx
@@ -0,0 +1,39 @@
+import React, { TextareaHTMLAttributes, useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+
+import './styles.css';
+
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  name: string;
+  label: string;
+}
+
+const Textarea: React.FC<TextareaProps> = ({ name, label, ...props }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <div className="textarea-block">
+      <label htmlFor={name}>
+        {label}
+        <textarea
+          id={name}
+          name={name}
+          ref={inputRef}
+          defaultValue={defaultValue}
+          {...props}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default Textarea;
