cb872a8aca9fece22d0cfc21a48e43ffad7c4abf
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-03 10:22:41 -0300

    Use only to show the logo

--- src/components/Proffy/index.tsx
+++ src/components/Proffy/index.tsx
@@ -1,35 +1,19 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 
 import Logo from '../../assets/images/logo.svg';
 import { Container } from './styles';
 
-interface ProffyProps {
-  src?: string;
-  subtitle?: boolean | JSX.Element;
-  useDefault?: boolean;
-}
-
-function Proffy({
-  src,
-  subtitle = false,
-  useDefault = true,
-  children,
-}: PropsWithChildren<ProffyProps>) {
+function Proffy() {
   return (
     <Container>
       <div>
-        <img src={src ?? Logo} alt="Proffy" />
+        <img src={Logo} alt="Proffy" />
 
-        {subtitle ||
-          (useDefault && (
-            <h2>
-              Sua plataforma de
-              <br />
-              estudos online.
-            </h2>
-          ))}
-
-        {children}
+        <h2>
+          Sua plataforma de
+          <br />
+          estudos online.
+        </h2>
       </div>
     </Container>
   );
