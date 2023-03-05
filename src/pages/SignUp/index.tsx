1eac057345ce0ffe6d46b0885f6a29a959000aeb
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-05 14:20:07 -0300

    Create signup page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,21 @@
+import React, { useCallback, useRef, useState } from 'react';
+import Proffy from '../../components/Proffy';
+import BackButton from '../../components/BackButton';
+import { Container, Main } from '../../assets/styles/global';
+
+function SignUp() {
+  return (
+    <Container>
+      <Main>
+        <div>
+          <BackButton />
+          <div>
+        </div>
+      </Main>
+
+      <Proffy />
+    </Container>
+  );
+}
+
+export default SignUp;
