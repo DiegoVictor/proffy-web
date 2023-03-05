c1a7db9917734a7c2b4a23f5c32b8c9a65b44788
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-05 14:23:59 -0300

    Handle signup form data

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,10 +2,14 @@ import React, { useCallback, useRef, useState } from 'react';
 import { Form } from '@unform/web';
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Title, InputGroup, Bottom, Subtitle } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
 import Proffy from '../../components/Proffy';
 import BackButton from '../../components/BackButton';
 import { Container, Main } from '../../assets/styles/global';
@@ -15,6 +19,44 @@ function SignUp() {
   const [hidePassword, setHidePassword] = useState(true);
   const [successOverlay, setSuccessOverlay] = useState(false);
 
+  const handleSubmit = useCallback(
+    async ({ email, password, name, surname }) => {
+      try {
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Digite um email válido')
+            .required('Email obrigatório'),
+          password: Yup.string().required('Senha obrigatória'),
+          name: Yup.string().required('Nome é obrigatório'),
+          surname: Yup.string().required('Sobrenome é obrigatório'),
+        });
+
+        await schema.validate(
+          { name, surname, email, password },
+          { abortEarly: false },
+        );
+
+        await api.post('/users', {
+          name,
+          surname,
+          email,
+          password,
+        });
+
+        setSuccessOverlay(true);
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          form.current?.setErrors(errors);
+        } else {
+          toast.error(
+            'Ocorreu um erro ao tentar cadastrar seus dados, tente novamente.',
+          );
+        }
+      }
+    },
+    [form],
+  );
   return (
     <Container>
       <Main>
