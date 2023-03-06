b05444f6fdcccb8d4873fe06d97ec1ae8d379897
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-06 10:26:00 -0300

    Create signin page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,21 @@
+import React, { useCallback, useRef, useState } from 'react';
+import Proffy from '../../components/Proffy';
+import { Container, Main } from '../../assets/styles/global';
+
+function SignIn() {
+  return (
+    <Container>
+      <Proffy />
+
+      <Main>
+        <div>
+          <div />
+
+          <div>
+        </div>
+      </Main>
+    </Container>
+  );
+}
+
+export default SignIn;
