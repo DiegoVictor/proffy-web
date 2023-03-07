bfd40ca7e7f39a63f2f5e00146fd0c563589bb6e
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-07 10:02:50 -0300

    Create signin form

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,26 @@
 import React, { useCallback, useRef, useState } from 'react';
+import { Form } from '@unform/web';
 import { MdFavorite } from 'react-icons/md';
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import { Footer, Register } from './styles';
+import { FormHandles } from '@unform/core';
+
+import Input from '../../components/Input';
+import { useAuth } from '../../hooks/auth';
 import Proffy from '../../components/Proffy';
 import { Container, Main } from '../../assets/styles/global';
 
 function SignIn() {
+  const form = useRef<FormHandles>(null);
+  const [hidePassword, setHidePassword] = useState(true);
+  const { signIn } = useAuth();
+
+  const handleSubmit = useCallback(
+    async ({ email, password, remember }) => {
+    },
+    [form, signIn],
+  );
+
   return (
     <Container>
       <Proffy />
@@ -15,6 +30,37 @@ function SignIn() {
           <div />
 
           <div>
+            <Title>Fazer login</Title>
+            <Form ref={form} onSubmit={handleSubmit}>
+              <InputGroup>
+                <Input name="email" placeholder="E-mail" />
+                <Input
+                  name="password"
+                  type={hidePassword ? 'password' : 'text'}
+                  placeholder="Senha"
+                  icon={
+                    !hidePassword ? (
+                      <AiOutlineEyeInvisible
+                        size="22"
+                        color="#8257E5"
+                        onClick={() => {
+                          setHidePassword(true);
+                        }}
+                      />
+                    ) : (
+                      <AiOutlineEye
+                        size="22"
+                        onClick={() => {
+                          setHidePassword(false);
+                        }}
+                      />
+                    )
+                  }
+                />
+              </InputGroup>
+            </Form>
+          </div>
+
           <Footer>
             <Register>
               Não tem conta?
