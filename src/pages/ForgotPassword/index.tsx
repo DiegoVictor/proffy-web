0dbe8b1bd33cc0d458372e6f09764c7658b0b763
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-08 09:23:52 -0300

    Show reset email sent message

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,9 +1,27 @@
 import React, { useCallback, useRef, useState } from 'react';
 import Proffy from '../../components/Proffy';
 import BackButton from '../../components/BackButton';
+import SuccessOverlay from '../../components/SuccessOverlay';
 import { Container, Main } from '../../assets/styles/global';
 
 function ForgotPassword() {
+  const [successOverlay, setSuccessOverlay] = useState(false);
+
+  if (successOverlay) {
+    return (
+      <SuccessOverlay
+        subtitle={<h1>Redefinição enviada!</h1>}
+        message={
+          <>
+            Boa, agora é só checar o e-mail que foi enviado para você
+            <br />
+            redefinir sua senha e aproveitar os estudos.
+          </>
+        }
+        buttonLabel="Voltar ao login"
+      />
+    );
+  }
   return (
     <Container>
       <Main>
