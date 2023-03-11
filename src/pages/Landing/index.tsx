5365ea7961d00bda0e7f2fa2b5604dfc13f1f8c6
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-11 00:34:45 -0300

    Wrap page content

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,7 +7,7 @@ import StudyIcon from '../../assets/images/icons/study.svg';
 import GiveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import PurpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import api from '../../services/api';
-import './styles.css';
+import { Container, Header, Content } from './styles';
 
 function Landing() {
   const [totalConnections, setTotalConnections] = useState(0);
@@ -20,20 +20,12 @@ function Landing() {
   }, []);
 
   return (
-    <div id="page-landing">
-      <div id="page-landing-content" className="container">
-        <div className="logo-container">
-          <img src={LogoImg} alt="Proffy" />
-          <h2>Sua plataforma de estudos online.</h2>
+    <Container>
+      <Header>
         </div>
+      </Header>
 
-        <img
-          src={LandingImg}
-          alt="Plataforma de Estudos"
-          className="hero-image"
-        />
-
-        <div className="buttons-container">
+      <Content>
           <Link to="/study" className="study">
             <img src={StudyIcon} alt="Estudar" />
             Estudar
@@ -44,13 +36,8 @@ function Landing() {
             Dar Aulas
           </Link>
         </div>
-
-        <span className="total-connections">
-          Total de {totalConnections} conexões já realizadas
-          <img src={PurpleHeartIcon} alt="Coração roxo" />
-        </span>
-      </div>
-    </div>
+      </Content>
+    </Container>
   );
 }
 
