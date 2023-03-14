f0de7dce3cc4ded38b0fbb2b3b87bf8f9c20b2bd
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-14 09:38:34 -0300

    Accept more attributes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,15 +5,17 @@ import { Container } from './styles';
 interface ButtonProps {
   onClick?: () => void;
   bg?: string;
+  type?: 'button' | 'submit';
 }
 
 function Button({
   children,
   onClick,
+  type = 'button',
   ...props
 }: PropsWithChildren<ButtonProps>) {
   return (
-    <Container type="button" onClick={onClick} {...props}>
+    <Container type={type} onClick={onClick} {...props}>
       {children}
     </Container>
   );
