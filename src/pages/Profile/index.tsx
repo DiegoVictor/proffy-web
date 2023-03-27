8623f0a654159e19c282e42341359d73bcefbf3b
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-27 00:16:49 -0300

    Show user name and avatar

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -10,6 +10,11 @@ function Profile() {
           page: 'Meu perfil',
         }}
       >
+        <div>
+          <Avatar src={user?.avatar} alt={user?.name} />
+          <h2>{user?.name}</h2>
+          <h3>{subject}</h3>
+        </div>
       </Header>
     </Container>
   );
