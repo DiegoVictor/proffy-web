1d6720c0a0707ea39e0ba59a1079cb0eca8dc629
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-27 00:15:06 -0300

    Add page header

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,17 @@
+import Header from '../../components/Header';
+
 function Profile() {
+  return (
+    <Container>
+      <Header
+        options={{
+          title: '',
+          page: 'Meu perfil',
+        }}
+      >
+      </Header>
+    </Container>
+  );
 }
 
 export default Profile;
