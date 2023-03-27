057cd0e3c4e29e543286a5f66bca0e7fe264e691
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-27 00:17:55 -0300

    Retrieve user data

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,6 +2,22 @@ import { Container, Avatar } from './styles';
 import Header from '../../components/Header';
 
 function Profile() {
+  const [user, setUser] = useState<User>();
+  const { user: { id: userId } = {} } = useAuth();
+  const [subject, setSubject] = useState<string>('');
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get(`/users/${userId}`);
+      setUser(data);
+    })();
+  }, [userId]);
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get('/classes/my-class');
+      setSubject(data.subject);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header
