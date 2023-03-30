9d55ed12171d4d7e8511dba94774684a8f3a0d76
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-30 18:53:54 -0300

    Overwrite some header styles

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -3,4 +3,14 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100vh;
   width: 100vw;
+
+  header {
+    > section {
+      padding-bottom: 0px;
+      margin: 64px auto calc(3.2rem + 64px);
+    }
+  }
+`;
+
+export const Main = styled.div`
 `;
